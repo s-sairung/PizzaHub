8797639a6d567eabb4e3e2fcf453be0fa005be82
s-sairung <siwakorn_pipe@hotmail.com>
2020-04-01 20:39:35 +0700

    Add register fragment

--- app/src/main/java/chula/project/pizzahub/ProfileFragment.java
+++ app/src/main/java/chula/project/pizzahub/ProfileFragment.java
@@ -4,16 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        Button registerButton = (Button) view.findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(this);
+        return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.registerButton:
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileRegisterFragment()).commit();
+//                getFragmentManager().beginTransaction().addToBackStack(null);
+//                getFragmentManager().beginTransaction().commit();
+                break;
+        }
+    }
+
 }
