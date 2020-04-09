d1d822c6b96007e91fba972d5becbb0a714d5ef8
noddragon1 <noddragon1@hotmail.com>
2020-04-09 21:44:30 +0700

    ลองเปลี่ยนbuttonเป็นbuttonimageค่อนข้างดีแต่ลองทำกดเปลี่ยนคลาสไม่ได้ไม่รู้ทำไม เพิ่มคลาสPizzaFragment,SetComboFragment,fragment_pizza,fragment_setcombo(เพิ่มมาแบบไม่ค่อยมีประโยชน์ไปต่อไม่ได้;-;)

--- app/src/main/java/chula/project/pizzahub/HomeFragment.java
+++ app/src/main/java/chula/project/pizzahub/HomeFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import chula.project.pizzahub.PizzaFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -17,9 +20,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-
-
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
+    public void flash(View v){
+        Intent itnt = new Intent(this,PizzaFragment);
+        startActivity(itnt);
+    }
 }
