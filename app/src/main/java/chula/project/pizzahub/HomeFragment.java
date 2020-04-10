43215b06ca55017e7c84b9266397b9cd61065e3e
s-sairung <siwakorn_pipe@hotmail.com>
2020-04-10 15:18:48 +0700

    เพิ่ม back stack ให้หน้า pizza จะได้กดปุ่มกลับได้ เย่ ๆ

--- app/src/main/java/chula/project/pizzahub/HomeFragment.java
+++ app/src/main/java/chula/project/pizzahub/HomeFragment.java
@@ -5,33 +5,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import chula.project.pizzahub.PizzaFragment;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        //clear back stack
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        for(int i = 0; i < fm.getBackStackEntryCount(); ++i) {
+            fm.popBackStack();
+        }
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ImageButton imageButton = (ImageButton) view.findViewById(R.id.imageButtonFlash);
+        imageButton.setOnClickListener(this);
+
+        return view;
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.imageButtonFlash:
-//                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileSignupFragment()).commit();
                 Fragment newFragment = new PizzaFragment();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 transaction.replace(R.id.fragment_container, newFragment);
-//                transaction.addToBackStack(null);
+                transaction.addToBackStack(null);
                 transaction.commit();
                 break;
         }
