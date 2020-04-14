db1f7b62760f2766162447d5832de0100f63469b
s-sairung <siwakorn_pipe@hotmail.com>
2020-04-14 20:15:33 +0700

    ลบไฟล์ combo ทิ้ง พึ่งเห็นว่ามีอยู่แล้ว

--- app/src/main/java/chula/project/pizzahub/classes/FragmentAssist.java
+++ app/src/main/java/chula/project/pizzahub/classes/FragmentAssist.java
@@ -2,10 +2,10 @@ package chula.project.pizzahub.classes;
 
 import androidx.fragment.app.Fragment;
 
-import chula.project.pizzahub.ComboFragment;
 import chula.project.pizzahub.FamilyFragment;
 import chula.project.pizzahub.OthersFragment;
 import chula.project.pizzahub.PizzaFragment;
+import chula.project.pizzahub.SetComboFragment;
 
 public class FragmentAssist {
 
@@ -14,7 +14,7 @@ public class FragmentAssist {
         switch (cat) {
             case "Pizza": fragment = new PizzaFragment(); break;
             case "Others": fragment = new OthersFragment(); break;
-            case "Combo Set": fragment = new ComboFragment(); break;
+            case "Combo Set": fragment = new SetComboFragment(); break;
             case "Family Set": fragment = new FamilyFragment(); break;
             default: fragment = new OthersFragment(); break;
         }
